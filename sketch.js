22c7dcbb7d77684cadddd7277ea710c4a1bbbdea
FazeDragon <74134721+FazeDragon@users.noreply.github.com>
2021-02-16 14:28:32 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,120 @@
+const Engine  = Matter.Engine;
+const World = Matter.World;
+const Bodies =  Matter.Bodies
+const Constraint = Matter.Constraint;
+
+var box1,box2,box3,box4,box5, box6,box7,box8,box9,box10;
+var box11,box12,box13,box14,box15,box16,box17,box18,box19,box20;
+var box21,box22,box23,box24,box25;
+
+
+
+var polygon,polygon_img , ground , ground1,ground2;
+
+var engine , world;
+
+function setup(){
+
+createCanvas(1000, 750);
+
+
+engine = Engine.create();
+world = engine.world;
+
+
+ball = new Ball(100,325,50,50)
+slingshot = new SlingShot(ball.body,{x:50, y:325});
+
+box1 = new Box(630, 235,30,40, "cyan");
+    box2 = new Box(660, 235,30,40, "cyan");
+    box3 = new Box(690, 235,30,40, "cyan");
+    box4 = new Box(720, 235,30,40, "cyan");
+    box5 = new Box(750, 235,30,40, "cyan");
+    box6 = new Box(780, 235,30,40, "cyan");
+    box7 = new Box(810, 235,30,40, "cyan");
+
+    box8 = new Box(660, 195,30,40, "pink");
+    box9 = new Box(690, 195,30,40, "pink");
+    box10 = new Box(720, 195,30,40, "pink");
+    box11 = new Box(750, 195,30,40, "pink");
+    box12 = new Box(780, 195,30,40, "pink");
+    
+    box13 = new Box(690, 155,30,40, "lightblue");
+    box14 = new Box(720, 155,30,40, "lightblue");
+    box15 = new Box(750, 155,30,40, "lightblue");
+
+    box16 = new Box(720 , 115 , 30,40 , "lightgrey"); 
+
+    box17 = new Box(260, 370,30,40, "pink");
+    box18 = new Box(290, 370,30,40, "pink");
+    box19 = new Box(320, 370,30,40, "pink");
+    box20 = new Box(350, 370,30,40, "pink");
+    box21 = new Box(380, 370,30,40, "pink");
+
+    box22 = new Box(290, 330,30,40, "lightblue");
+    box23 = new Box(320, 330,30,40, "lightblue");
+    box24 = new Box(350, 330,30,40, "lightblue");
+    box25 = new Box(320, 290,30,40, "lightgrey");
+ground = new Ground(500,600,1000,10);
+	
+ground1 = new Ground(720,275,250,10);
+ground2 = new Ground(320,400,200,10);
+
+Engine.run(engine);
+
+}
+function draw(){
+
+    background(0);
+
+    Engine.update(engine);
+    
+    ball.display();
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+
+    box13.display();
+    box14.display();
+    box15.display();
+
+    box16.display();
+
+    box17.display();
+    box18.display();
+    box19.display();
+    box20.display();
+    box21.display();
+
+    box22.display();
+    box23.display();
+    box24.display(); 
+
+    box25.display();
+
+    ground.display();
+    ground1.display();
+    ground2.display();
+    slingshot.display();
+
+  //  imageMode(CENTER)
+   // image(polygon_img,polygon.position.x,polygon.position.y,40,40);
+
+}
+
+   function mouseDragged(){
+      Matter.Body.setPosition(ball.body,{x: mouseX, y:mouseY})
+   }
+   function mouseReleased(){
+       slingshot.fly();
+   }
